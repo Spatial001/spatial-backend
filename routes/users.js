21ad5f6c11f18472e73fe27e3ffba12e8a845ef7
Chandan Kumar Shaw <chandan.kumar.shaw@sap.com>
2024-03-22 23:23:39 +0530

    Refactor

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,7 @@
 import express from 'express'
-import { jwtAuth, validation } from '../middleware/validator.js';
 import { login, signup , home } from '../controllers/users.js'
 const router = express.Router();
 router.post('/login', login)
 router.post('/signup', signup)
-router.post('/home', jwtAuth, home)
+router.post('/home', home)
 export default router;
